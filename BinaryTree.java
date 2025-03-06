66fc84e9d33055df2ad828d7b60a906609b2bec3
k-vimla <vimlakushwaha56@gmail.com>
2025-03-06 15:45:20 +0530

    First Java Program

---
+++ BinaryTree.java
@@ -0,0 +1,17 @@
+class BinaryTree{
+	public BinaryTree(){
+		
+	}
+	/*
+	private static class Node{
+		int val;
+		Node left;
+		Node right;
+		public Node(int value){
+			this.value = value; 
+		}
+	}*/
+	public static void main(String[] args){
+		System.out.println("Hello Vimla");
+	}
+}
