6095d531f00654a2cbe9ffc1c335eded9bc6873f
k-vimla <vimlakushwaha56@gmail.com>
2025-03-06 16:37:59 +0530

    Scanner class taking int input

--- BinaryTree.java
+++ BinaryTree.java
@@ -2,7 +2,7 @@ class BinaryTree{
 	public BinaryTree(){
 		
 	}
-	/*
+
 	private static class Node{
 		int val;
 		Node left;
@@ -10,7 +10,15 @@ class BinaryTree{
 		public Node(int value){
 			this.value = value; 
 		}
-	}*/
+	}
+
+	private Node root{
+		// insert element
+		public void populate(Scanner scanner){
+			System.out.println("Enter the root node value");
+			int value = scanner.nextInt();
+		}
+	}
 	public static void main(String[] args){
 		System.out.println("Hello Vimla");
 	}
---
+++ ScText.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class ScText{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		Syetem.out.println("Enter a number: ");
+		int num = sc.nextInt();
+		System.out.println("You Entered: "+num);
+		sc.close();
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
