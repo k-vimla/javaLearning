a982b73f00ade580e9232e2298f1de3ba8b96f0f
k-vimla <vimlakushwaha56@gmail.com>
2025-03-06 18:01:02 +0530

    binary Tree default implemetnation done

--- ScText.java
+++ ScText.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class ScText{
 	public static void main(String[] args){
 		Scanner sc = new Scanner(System.in);
-		Syetem.out.println("Enter a number: ");
+		System.out.println("Enter a number: ");
 		int num = sc.nextInt();
 		System.out.println("You Entered: "+num);
 		sc.close();
